905d1a80cd46d005f0fda4bda6a7f8125b7b00a2
Hiba Hasan <94163709+hibahasan998@users.noreply.github.com>
2022-05-27 06:51:48 +0300

    add lazy  loading for page1 and page2 modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/page1/page1.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Page1Component } from './page1.component';
+
+
+
+@NgModule({
+  declarations: [
+    Page1Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class Page1Module { }
